b03291999fd7c1973d8c56c0465b5810108c62ca
Ruth Obe <ruthobey@gmail.com>
2020-02-16 21:21:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   
   creator_hash = {
-    :key => value 
+    creator_hash[key] => value 
   }
   
   return creator_hash
